3e682e1c668855b79c20f7706fc7791c86f72f5b
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2025-08-21 08:35:45 +0800

    Introduce LRU miss_strategy.rb

---
+++ actionpack/lib/action_dispatch/routing/lru_strategies/miss_strategy.rb
@@ -0,0 +1,12 @@
+module ActionDispatch
+  module Routing
+    module LruStrategies
+      class MissStrategy
+        def call(lru, _key)
+          lru.increment_misses
+          nil
+        end
+      end
+    end
+  end
+end
